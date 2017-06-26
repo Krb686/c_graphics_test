55abd82a1f339bfd63e2f1a1b4010276ef8e5e72
krb686@gmail.com <krb686@gmail.com>
2017-06-25 23:31:49 -0400

    More complex ui example, and some cairo shapes

--- src/gtk_cairo_gallery.c
+++ src/gtk_cairo_gallery.c
@@ -1,5 +1,22 @@
 #include <cairo.h>
 #include <gtk/gtk.h>
+#include <math.h>
+
+// TODO - custom objects
+//  - codebox
+//  - arrow
+// TODO - dashed lines
+// TODO - line caps
+
+// ---- Paths ----
+// cairo_arc
+// cairo_arc_negative
+// cairo_rectangle
+//
+//cairo_stroke
+//cairo_stroke_preserve
+//
+
 
 
 static void update_drawing(cairo_t *);
@@ -10,7 +27,61 @@ static gboolean draw_handler(GtkWidget *widget, cairo_t *cr, gpointer user_data)
 }
 
 static void update_drawing(cairo_t *cr){
+    double TWOPI = 2*M_PI;
 
+    cairo_set_source_rgb(cr, 0, 0, 0);
+    
+
+    // Example 1 - text
+    cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_font_size(cr, 20.0);
+    cairo_move_to(cr, 10.0, 20.0);
+    cairo_show_text(cr, "Hello!");
+
+    // Example 2 - lines
+    cairo_set_line_width(cr, 1.0);
+    cairo_move_to(cr, 20, 40);
+    cairo_line_to(cr, 40, 40);
+    cairo_stroke(cr);
+
+    // Example 3 - arc
+    cairo_arc(cr, 20, 80, 5, 0, (180*(M_PI / 180)));
+    cairo_stroke(cr);
+
+    // Example 4 - arc_negative
+    cairo_arc_negative(cr, 40, 80, 5, 0, (180*(M_PI/180)));
+    cairo_stroke(cr);
+
+    // Example 5 - Bezier spline
+    cairo_move_to(cr, 20, 100);
+    cairo_curve_to(cr, 40, 130, 60, 110, 60, 100);
+    cairo_stroke(cr);
+
+    // Example 6 - Rectangle
+    cairo_rectangle(cr, 20, 160, 60, 20);
+    cairo_stroke(cr);
+
+    // Example 7 - Fill
+    cairo_arc(cr, 20, 200, 10, 0, TWOPI);
+    cairo_stroke_preserve(cr);
+    cairo_set_source_rgb(cr, 0.6, 0.6, 0.2);
+    cairo_fill(cr);
+
+    // Codebox
+    cairo_set_source_rgb(cr, 1, 1, 1);
+    cairo_rectangle(cr, 400, 200, 300, 100);
+    cairo_stroke_preserve(cr);
+    cairo_set_source_rgb(cr, 0.7, 0.7, 0.7);
+    cairo_fill(cr);
+
+
+    cairo_arc(cr, 800, 600, 15, 0, TWOPI);
+    cairo_stroke_preserve(cr);
+    cairo_fill(cr);
+
+    cairo_move_to(cr, 550, 300);
+    cairo_curve_to(cr, 550, 600, 750, 600, 785, 600);
+    cairo_stroke(cr);
 }
 
 static gint handle_opts(GtkApplication *app, GVariantDict *opts, gpointer user_data){
@@ -22,6 +93,7 @@ static void setup_app(GtkApplication *app, gpointer user_data){
     printf("Setting up the application\n");
     GtkApplicationWindow *window;
     GtkBuilder *builder;
+    GtkDrawingArea *drawbox;
 
     // Create the GTK builder
     builder = gtk_builder_new_from_file("builder.ui");
@@ -30,6 +102,10 @@ static void setup_app(GtkApplication *app, gpointer user_data){
     window = gtk_builder_get_object(builder, "window1");
     gtk_window_set_title(GTK_WINDOW(window), "Cairo Gallery");
     gtk_window_set_default_size(GTK_WINDOW(window), 1440, 900);
+
+    // Grab reference to the drawbox
+    drawbox = gtk_builder_get_object(builder, "drawingarea1");
+    g_signal_connect(G_OBJECT(drawbox), "draw", G_CALLBACK(draw_handler), NULL);
 
     gtk_window_set_application(GTK_WINDOW(window), GTK_APPLICATION(app));
     gtk_widget_show_all(window); 
